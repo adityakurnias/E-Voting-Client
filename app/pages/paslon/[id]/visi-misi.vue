dd6ae19decc1e11e57497c9d3c98c6948b0bd36e
adityakurnias <adityakurniasaputra903@gmail.com>
2025-09-21 21:49:21 +0700

    style(visimisi): Add dynamic background

--- app/pages/paslon/[id]/visi-misi.vue
+++ app/pages/paslon/[id]/visi-misi.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
+  <div
+    :style="backgroundStyle"
+    class="min-h-screen relative overflow-hidden bg-cover bg-no-repeat">
     
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+const route = useRoute();
+const candidateId = route.params.id;
 
+const backgroundStyle = computed(() => {
+  const validIds = [1, 2, 3];
+  const id = parseInt(candidateId);
+
+  if (validIds.includes(id)) {
+    return {
+      backgroundImage: `url('/images/Background/VISIMISI/Paslon-${id}.png')`,
+    };
+  }
+});
 </script>
-
-<style>
-
-</style>
