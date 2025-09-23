f71859ae0411a24bd9994b972cda45325447a8f6
adityakurnias <adityakurniasaputra903@gmail.com>
2025-09-23 17:22:38 +0700

    fix(login): Fix landing page  appear in login

--- app/app.vue
+++ app/app.vue
@@ -2,8 +2,21 @@
   <div class="bg-[#2a3944] min-h-screen">
     <NuxtRouteAnnouncer />
     <NuxtLoadingIndicator />
-    <NuxtLayout>
+    <NuxtLayout v-if="isAuthReady">
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useAuth } from '~/composables/Auth';
+
+const isAuthReady = ref(false);
+const { initializeAuth } = useAuth();
+
+onMounted(() => {
+  initializeAuth();
+  isAuthReady.value = true;
+});
+</script>
